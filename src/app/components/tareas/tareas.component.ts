23f1027351b83328f0a61d1f6b92861b25c18a84
Axel <axeelleguero@gmail.com>
2024-11-18 20:05:53 -0300

    Me falto el componente

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { TareaService, Tarea } from '../../services/tarea.service';
+
+
+@Component({
+  selector: 'app-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './tareas.component.html',
+  styleUrl: './tareas.component.css'
+})
+export class TareasComponent implements OnInit{
+  tareas: Tarea[] = [];
+  nuevaTarea: Tarea = { titulo: '', descripcion: '', completada: false, prioridad: '' };
+
+  constructor(private tareaService: TareaService) {}
+
+  ngOnInit(): void {
+    this.obtenerTareas();
+  }
+
+  obtenerTareas(): void {
+    this.tareaService.obtenerTareas().subscribe((tareas) => (this.tareas = tareas));
+  }
+  agregarTarea(): void {
+    this.tareaService.crearTarea(this.nuevaTarea).subscribe(() => {
+      this.obtenerTareas();
+      this.nuevaTarea = { titulo: '', descripcion: '', completada: false, prioridad: '' };
+    });
+  }
+
+  eliminarTarea(id: string): void {
+    this.tareaService.eliminarTarea(id).subscribe(() => this.obtenerTareas());
+  }
+}
